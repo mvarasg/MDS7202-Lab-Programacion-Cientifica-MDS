9eaac53e3981e7d28487b58002c79abe8db528fb
martinsep <93410300+martinsep@users.noreply.github.com>
2023-07-12 22:07:43 -0400

    Kedro project added

---
+++ lab11/kedro-lab11/src/kedro_lab11/pipelines/data_prep/pipeline.py
@@ -0,0 +1,37 @@
+"""
+This is a boilerplate pipeline 'data_prep'
+generated using Kedro 0.18.11
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import get_data, preprocess_companies, preprocess_shuttles, create_model_input_table
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=get_data,
+                inputs=None,
+                outputs=["companies","shuttles","reviews"],
+                name="get_data",
+            ),
+            node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="companies_prep",
+                name="prep_companies",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="shuttles_prep",
+                name="prep_shuttles",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["shuttles_prep","companies_prep","reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table",
+            )
+        ]
+    )
